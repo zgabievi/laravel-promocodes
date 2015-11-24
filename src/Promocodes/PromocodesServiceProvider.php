6bd42bfa1aa78a756a4ec9fb45327af2bb9b0885
Zura Gabievi <zura.gabievi@gmail.com>
2015-11-24 15:30:00 +0400

    init

---
+++ src/Promocodes/PromocodesServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace zgabievi\Promocodes;
+
+use Illuminate\Support\ServiceProvider;
+
+class PromocodesServiceProvider extends ServiceProvider
+{
+	/**
+	 * Bootstrap the application services.
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->publishes([
+			__DIR__ . '/../config/promocodes.php' => config_path('promocodes.php'),
+		]);
+	}
+
+	/**
+	 * Register the application services.
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->mergeConfigFrom(
+			__DIR__ . '/../config/promocodes.php', 'promocodes'
+		);
+
+		$this->app['promocodes'] = $this->app->share(
+			function () {
+				return new Promocodes();
+			}
+		);
+	}
+}
