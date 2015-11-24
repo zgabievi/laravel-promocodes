bb1d238951779289108cc57d480871992ec9364e
Zura Gabievi <zura.gabievi@gmail.com>
2015-11-24 15:51:12 +0400

    restructure:

---
+++ src/config/promocodes.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+	/*
+	 *
+	 */
+	'model' => \App\Promocode::class,
+
+	/*
+	 *
+	 */
+	'characters' => '23456789ABCDEFGHJKLMNPQRSTUVWXYZ',
+
+	/*
+	 *
+	 */
+	'prefix'     => 'START',
+
+	/*
+	 *
+	 */
+	'suffix'     => 'END',
+
+	/*
+	 *
+	 */
+	'mask'       => '****-****',
+
+	/**
+	 *
+	 */
+	'separator'  => '-',
+];
