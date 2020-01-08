77a475b04b5e6d6065a9f5e41d9463d9faefcf22
Siavash Habil <habil.amirkhiz@sony.com>
2020-01-08 17:26:55 +0300

    - Add static methods to Facade for code autocomplete and method argument details

--- src/Facades/Promocodes.php
+++ src/Facades/Promocodes.php
@@ -2,8 +2,24 @@
 
 namespace Gabievi\Promocodes\Facades;
 
+use Gabievi\Promocodes\Models\Promocode;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Promocodes
+ * @package Gabievi\Promocodes\Facades
+ * @method static array output(int $amount = 1)
+ * @method static Collection create(int $amount = 1, float $reward = null, array $data = [], int $expires_in = null, int $quantity = null, bool $is_disposable = false)
+ * @method static Collection createDisposable(int $amount = 1, float $reward = null, array $data = [], int $expires_in = null, int $quantity = null)
+ * @method static bool|Promocode check(string $code)
+ * @method static bool|Promocode apply(string $code)
+ * @method static bool|Promocode redeem(string $code)
+ * @method static bool disable(string $code)
+ * @method static void clearRedundant()
+ * @method static Promocode[]|\Illuminate\Database\Eloquent\Collection all()
+ * @method static bool isSecondUsageAttempt(Promocode $promocode)
+ */
 class Promocodes extends Facade
 {
     /**
