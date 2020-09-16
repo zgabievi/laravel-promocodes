5d37199de9a06f9931a8b60d387348b4733ca5a0
Zura Gabievi <zura.gabievi@singular.uk>
2020-09-17 02:33:03 +0400

    #48 - Support for custom prefix/suffix per generation

--- src/Promocodes.php
+++ src/Promocodes.php
@@ -11,6 +11,20 @@ use Gabievi\Promocodes\Exceptions\InvalidPromocodeException;
 class Promocodes
 {
     /**
+     * Prefix for code generation
+     *
+     * @var string
+     */
+    protected $prefix;
+
+    /**
+     * Suffix for code generation
+     *
+     * @var string
+     */
+    protected $suffix;
+
+    /**
      * Generated codes will be saved here
      * to be validated later.
      *
@@ -33,6 +47,85 @@ class Promocodes
     {
         $this->codes = Promocode::pluck('code')->toArray();
         $this->length = substr_count(config('promocodes.mask'), '*');
+
+        $this->prefix = (bool)config('promocodes.prefix')
+            ? config('promocodes.prefix') . config('promocodes.separator')
+            : '';
+
+        $this->suffix = (bool)config('promocodes.suffix')
+            ? config('promocodes.separator') . config('promocodes.suffix')
+            : '';
+    }
+
+    /**
+     * Save one-time use promocodes into database
+     * Successful insert returns generated promocodes
+     * Fail will return empty collection.
+     *
+     * @param int $amount
+     * @param null $reward
+     * @param array $data
+     * @param int|null $expires_in
+     * @param int|null $quantity
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function createDisposable(
+        $amount = 1,
+        $reward = null,
+        array $data = [],
+        $expires_in = null,
+        $quantity = null
+    )
+    {
+        return $this->create($amount, $reward, $data, $expires_in, $quantity, true);
+    }
+
+    /**
+     * Save promocodes into database
+     * Successful insert returns generated promocodes
+     * Fail will return empty collection.
+     *
+     * @param int $amount
+     * @param null $reward
+     * @param array $data
+     * @param int|null $expires_in
+     * @param bool $is_disposable
+     * @param int|null $quantity
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function create(
+        $amount = 1,
+        $reward = null,
+        array $data = [],
+        $expires_in = null,
+        $quantity = null,
+        $is_disposable = false
+    )
+    {
+        $records = [];
+
+        foreach ($this->output($amount) as $code) {
+            $records[] = [
+                'code' => $code,
+                'reward' => $reward,
+                'data' => json_encode($data),
+                'expires_at' => $expires_in ? Carbon::now()->addDays($expires_in) : null,
+                'is_disposable' => $is_disposable,
+                'quantity' => $quantity,
+            ];
+        }
+
+        if (Promocode::insert($records)) {
+            return collect($records)->map(function ($record) {
+                $record['data'] = json_decode($record['data'], true);
+
+                return $record;
+            });
+        }
+
+        return collect([]);
     }
 
     /**
@@ -60,95 +153,86 @@ class Promocodes
     }
 
     /**
-     * Save promocodes into database
-     * Successful insert returns generated promocodes
-     * Fail will return empty collection.
+     * Set custom prefix for next generation
      *
-     * @param int $amount
-     * @param null $reward
-     * @param array $data
-     * @param int|null $expires_in
-     * @param bool $is_disposable
-     * @param int|null $quantity
-     *
-     * @return \Illuminate\Support\Collection
+     * @param $prefix
+     * @return $this
      */
-    public function create(
-        $amount = 1,
-        $reward = null,
-        array $data = [],
-        $expires_in = null,
-        $quantity = null,
-        $is_disposable = false
-    ) {
-        $records = [];
-
-        foreach ($this->output($amount) as $code) {
-            $records[] = [
-                'code' => $code,
-                'reward' => $reward,
-                'data' => json_encode($data),
-                'expires_at' => $expires_in ? Carbon::now()->addDays($expires_in) : null,
-                'is_disposable' => $is_disposable,
-                'quantity' => $quantity,
-            ];
-        }
-
-        if (Promocode::insert($records)) {
-            return collect($records)->map(function ($record) {
-                $record['data'] = json_decode($record['data'], true);
-
-                return $record;
-            });
-        }
-
-        return collect([]);
+    public function setPrefix($prefix)
+    {
+        $this->prefix = $prefix;
+        return $this;
     }
 
     /**
-     * Save one-time use promocodes into database
-     * Successful insert returns generated promocodes
-     * Fail will return empty collection.
+     * Set custom suffix for next generation
      *
-     * @param int $amount
-     * @param null $reward
-     * @param array $data
-     * @param int|null $expires_in
-     * @param int|null $quantity
-     *
-     * @return \Illuminate\Support\Collection
+     * @param $suffix
+     * @return $this
      */
-    public function createDisposable(
-        $amount = 1,
-        $reward = null,
-        array $data = [],
-        $expires_in = null,
-        $quantity = null
-    ) {
-        return $this->create($amount, $reward, $data, $expires_in, $quantity, true);
+    public function setSuffix($suffix)
+    {
+        $this->suffix = $suffix;
+        return $this;
     }
 
     /**
-     * Check promocode in database if it is valid.
+     * Here will be generated single code using your parameters from config.
+     *
+     * @return string
+     */
+    private function generate()
+    {
+        $characters = config('promocodes.characters');
+        $mask = config('promocodes.mask');
+        $promocode = '';
+        $random = [];
+
+        for ($i = 1; $i <= $this->length; $i++) {
+            $character = $characters[rand(0, strlen($characters) - 1)];
+            $random[] = $character;
+        }
+
+        shuffle($random);
+        $length = count($random);
+
+        $promocode .= $this->prefix;
+
+        for ($i = 0; $i < $length; $i++) {
+            $mask = preg_replace('/\*/', $random[$i], $mask, 1);
+        }
+
+        $promocode .= $mask;
+        $promocode .= $this->suffix;
+
+        return $promocode;
+    }
+
+    /**
+     * Your code will be validated to be unique for one request.
+     *
+     * @param $collection
+     * @param $new
+     *
+     * @return bool
+     */
+    private function validate($collection, $new)
+    {
+        return !in_array($new, array_merge($collection, $this->codes));
+    }
+
+    /**
+     * Reedem promocode to user that it's used from now.
      *
      * @param string $code
      *
      * @return bool|Promocode
-     * @throws InvalidPromocodeException
+     * @throws AlreadyUsedException
+     * @throws UnauthenticatedException
      */
-    public function check($code)
+    public function redeem($code)
     {
-        $promocode = Promocode::byCode($code)->first();
-
-        if ($promocode === null) {
-            throw new InvalidPromocodeException;
-        }
-
-        if ($promocode->isExpired() || ($promocode->isDisposable() && $promocode->users()->exists()) || $promocode->isOverAmount()) {
-            return false;
-        }
-
-        return $promocode;
+        return $this->apply($code);
     }
 
     /**
@@ -192,17 +276,39 @@ class Promocodes
     }
 
     /**
-     * Reedem promocode to user that it's used from now.
+     * Check promocode in database if it is valid.
      *
      * @param string $code
      *
      * @return bool|Promocode
-     * @throws AlreadyUsedException
-     * @throws UnauthenticatedException
+     * @throws InvalidPromocodeException
      */
-    public function redeem($code)
+    public function check($code)
     {
-        return $this->apply($code);
+        $promocode = Promocode::byCode($code)->first();
+
+        if ($promocode === null) {
+            throw new InvalidPromocodeException;
+        }
+
+        if ($promocode->isExpired() || ($promocode->isDisposable() && $promocode->users()->exists()) || $promocode->isOverAmount()) {
+            return false;
+        }
+
+        return $promocode;
+    }
+
+    /**
+     * Check if user is trying to apply code again.
+     *
+     * @param Promocode $promocode
+     *
+     * @return bool
+     */
+    public function isSecondUsageAttempt(Promocode $promocode)
+    {
+        return $promocode->users()->wherePivot(config('promocodes.related_pivot_key', 'user_id'),
+            auth()->id())->exists();
     }
 
     /**
@@ -252,87 +358,5 @@ class Promocodes
         return Promocode::all()->filter(function (Promocode $promocode) {
             return !$promocode->isExpired() && !($promocode->isDisposable() && $promocode->users()->exists()) && !$promocode->isOverAmount();
         });
-    }
-
-    /**
-     * Here will be generated single code using your parameters from config.
-     *
-     * @return string
-     */
-    private function generate()
-    {
-        $characters = config('promocodes.characters');
-        $mask = config('promocodes.mask');
-        $promocode = '';
-        $random = [];
-
-        for ($i = 1; $i <= $this->length; $i++) {
-            $character = $characters[rand(0, strlen($characters) - 1)];
-            $random[] = $character;
-        }
-
-        shuffle($random);
-        $length = count($random);
-
-        $promocode .= $this->getPrefix();
-
-        for ($i = 0; $i < $length; $i++) {
-            $mask = preg_replace('/\*/', $random[$i], $mask, 1);
-        }
-
-        $promocode .= $mask;
-        $promocode .= $this->getSuffix();
-
-        return $promocode;
-    }
-
-    /**
-     * Generate prefix with separator for promocode.
-     *
-     * @return string
-     */
-    private function getPrefix()
-    {
-        return (bool)config('promocodes.prefix')
-            ? config('promocodes.prefix') . config('promocodes.separator')
-            : '';
-    }
-
-    /**
-     * Generate suffix with separator for promocode.
-     *
-     * @return string
-     */
-    private function getSuffix()
-    {
-        return (bool)config('promocodes.suffix')
-            ? config('promocodes.separator') . config('promocodes.suffix')
-            : '';
-    }
-
-    /**
-     * Your code will be validated to be unique for one request.
-     *
-     * @param $collection
-     * @param $new
-     *
-     * @return bool
-     */
-    private function validate($collection, $new)
-    {
-        return !in_array($new, array_merge($collection, $this->codes));
-    }
-
-    /**
-     * Check if user is trying to apply code again.
-     *
-     * @param Promocode $promocode
-     *
-     * @return bool
-     */
-    public function isSecondUsageAttempt(Promocode $promocode)
-    {
-        return $promocode->users()->wherePivot(config('promocodes.related_pivot_key', 'user_id'),
-            auth()->id())->exists();
     }
 }
--- tests/OutputRandomPromocodesTest.php
+++ tests/OutputRandomPromocodesTest.php
@@ -21,4 +21,36 @@ class OutputRandomPromocodesTest extends TestCase
 
         $this->assertCount(10, $promocodes);
     }
+
+    /** @test */
+    public function it_can_output_several_promocodes_with_prefix_as_array()
+    {
+        $promocodes = Promocodes::setPrefix('TEST')->output(10);
+
+        $this->assertCount(10, $promocodes);
+        $this->assertStringStartsWith('TEST', $promocodes[0]);
+        $this->assertStringStartsWith('TEST', $promocodes[9]);
+    }
+
+    /** @test */
+    public function it_can_output_several_promocodes_with_suffix_as_array()
+    {
+        $promocodes = Promocodes::setSuffix('END')->output(10);
+
+        $this->assertCount(10, $promocodes);
+        $this->assertStringEndsWith('END', $promocodes[0]);
+        $this->assertStringEndsWith('END', $promocodes[9]);
+    }
+
+    /** @test */
+    public function it_can_output_several_promocodes_with_prefix_and_suffix_as_array()
+    {
+        $promocodes = Promocodes::setPrefix('ABC')->setSuffix('XYZ')->output(10);
+
+        $this->assertCount(10, $promocodes);
+        $this->assertStringStartsWith('ABC', $promocodes[0]);
+        $this->assertStringStartsWith('ABC', $promocodes[9]);
+        $this->assertStringEndsWith('XYZ', $promocodes[0]);
+        $this->assertStringEndsWith('XYZ', $promocodes[9]);
+    }
 }
