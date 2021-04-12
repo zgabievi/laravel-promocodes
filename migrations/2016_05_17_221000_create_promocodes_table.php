fe3e8161860f48cee3e1799df2969dbd354e2642
Berat Kara <beratkarabusra@gmail.com>
2021-04-12 15:43:41 +0300

    Update 2016_05_17_221000_create_promocodes_table.php
    Migration made workable with Laravel 6

--- migrations/2016_05_17_221000_create_promocodes_table.php
+++ migrations/2016_05_17_221000_create_promocodes_table.php
@@ -14,27 +14,29 @@ class CreatePromocodesTable extends Migration
     {
         Schema::create('promocodes', function (Blueprint $table) {
             $table->increments('id');
-
             $table->string('code', 32)->unique();
             $table->double('reward', 10, 2)->nullable();
             $table->integer('quantity')->nullable();
-
             $table->text('data')->nullable();
-
             $table->boolean('is_disposable')->default(false);
             $table->timestamp('expires_at')->nullable();
         });
 
         Schema::create('promocode_user', function (Blueprint $table) {
             $table->increments('id');
-
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('promocode_id');
-
+            $table->integer('user_id')->unsigned();
+            $table->integer('promocode_id')->unsigned();
             $table->timestamp('used_at');
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('promocode_id')->references('id')->on('promocodes')->onDelete('cascade');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('promocode_id')
+                ->references('id')
+                ->on('promocodes')
+                ->onDelete('cascade');
         });
     }
 
@@ -45,7 +47,7 @@ class CreatePromocodesTable extends Migration
      */
     public function down()
     {
-        Schema::drop('promocode_user');
-        Schema::drop('promocodes');
+        Schema::dropIfExists('promocode_user');
+        Schema::dropIfExists('promocodes');
     }
 }
