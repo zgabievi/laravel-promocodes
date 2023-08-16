3ceec410342202ffa98a1d52dc8aa81a1c84bc12
= <=>
2023-08-16 15:27:25 +0200

    fix decrementing usages_left when promo is unlimited

--- src/Promocodes.php
+++ src/Promocodes.php
@@ -251,7 +251,9 @@ class Promocodes
             event(new GuestAppliedPromocode($this->promocode));
         }
 
-        $this->promocode->decrement('usages_left');
+        if (!$this->promocode->isUnlimited()) {
+            $this->promocode->decrement('usages_left');
+        }
 
         return $this->promocode;
     }
